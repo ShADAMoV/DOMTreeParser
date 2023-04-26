71125259984ac1c0e791d0180fdf29a36e461df1
Shadnev Adam <84129656+ShADAMoV@users.noreply.github.com>
2023-04-26 14:28:35 +0300

    Add files via upload

---
+++ TreeStore.ts
@@ -0,0 +1,81 @@
+interface Item {
+    id: number | string;
+    parent: number | string;
+    type?: string | null;
+}
+
+class TreeStore {
+    tree: Item[] = [];
+    treeIndexedByIds: Map<Item['id'], Item>;
+    childrensByParentId: Map<Item['parent'], Item[]>
+
+    constructor(tree: Item[]) {
+        this.tree = tree;
+        this.treeIndexedByIds = tree.reduce((acc: Map<Item['id'], Item>, treeIndexedById) => {
+            acc.set(treeIndexedById.id, treeIndexedById);
+            return acc;
+        }, new Map());
+        this.childrensByParentId = tree.reduce((acc: Map<Item['parent'], Item[]>, childrenByParentId) => {
+            if (acc.has(childrenByParentId.parent)) {
+                acc.get(childrenByParentId.parent)?.push(childrenByParentId)
+            } else {
+                acc.set(childrenByParentId.parent, [childrenByParentId])
+            }
+            return acc;
+        }, new Map())
+    }
+
+    // Возвращает исходный массив
+    getAll(): Item[] {
+        return this.tree;
+    }
+
+    // Возвращает элемент по индексу
+    getItem(id: Item['id']): Item | undefined {
+        return this.treeIndexedByIds.get(id)
+    }
+
+    // Возвращает массив потомков элемента с переданным id
+    getChildren(id: Item['id']): Item[] {
+        return this.childrensByParentId.get(id) ?? []
+    }
+
+    // Возвращает массив потомков элемента с переданным id + потомков потомка и т.д.
+    getAllChildren(id: Item['id']): Item[] {
+        let result = this.getChildren(id);
+
+        for (const children of result) {
+            if (this.getChildren(children.id)) {
+                result = result.concat(this.getAllChildren(children.id))
+            }
+        }
+
+        return result;
+    }
+
+    // Возвращает родителя элемента по id
+    // P.S. Мне уже пора было укладываться спать, так как завтра ждут дела, из-за чего не смог закончить данный метод с использованием TS
+    // P.S.S. Я закомментил то, что по-идее бы выполняло требуемый функционал, но TS решил меня огорчить...
+    // getAllParents(id) {
+    //     let result = this.getItem(this.getItem(id).parent)
+    //     while (result.parent !== 'root') {
+    //         result = result.concat(this.getAllParents(result.id))
+    //     }
+    //     return result
+    // }
+}
+
+const items = [
+    { id: 1, parent: 'root' },
+    { id: 2, parent: 1, type: 'test' },
+    { id: 3, parent: 1, type: 'test' },
+
+    { id: 4, parent: 2, type: 'test' },
+    { id: 5, parent: 2, type: 'test' },
+    { id: 6, parent: 2, type: 'test' },
+
+    { id: 7, parent: 4, type: null },
+    { id: 8, parent: 4, type: null },
+];
+
+const ts = new TreeStore(items);
